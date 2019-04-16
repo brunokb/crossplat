a64c70b862918a01eb6b50748fa798c647688f71
Bruno Silva <bruno.k.silva@gmail.com>
2019-04-16 20:36:22 +1000

    V1

---
+++ BlankCordovaApp1/BlankCordovaApp1/www/scripts/example.js
@@ -0,0 +1,29 @@
+﻿// example.js file
+// Wait for device API libraries to load
+//
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+// device APIs are available
+//
+function onDeviceReady() {
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+    // Add similar listeners for other events
+}
+
+function onPause() {
+    console.log("onPause");
+}
+
+function onResume() {
+    console.log("onResume");
+}
+
+function onMenuKeyDown() {
+    // Handle the menubutton event
+}
+
+// Add similar event handlers for other events
